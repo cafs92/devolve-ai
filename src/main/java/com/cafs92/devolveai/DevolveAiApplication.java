2e6cd135131e9494d6020c35704b36a631382fae
Anderson Sales <saycor_13_gpb@hotmail.com>
2021-02-09 10:44:31 -0300

    Initial project configuration

---
+++ src/main/java/com/cafs92/devolveai/DevolveAiApplication.java
@@ -0,0 +1,13 @@
+package com.cafs92.devolveai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevolveAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevolveAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cafs92/devolveai/DevolveAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafs92.devolveai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevolveAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
