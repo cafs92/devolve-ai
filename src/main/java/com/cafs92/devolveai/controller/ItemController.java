3e4378dfa40de5f8315f5a90392bbc3496ecfa49
Anderson Sales <saycor_13_gpb@hotmail.com>
2021-02-09 13:56:05 -0300

    POST method for Item

--- src/main/java/com/cafs92/devolveai/controller/ItemController.java
+++ src/main/java/com/cafs92/devolveai/controller/ItemController.java
@@ -3,11 +3,9 @@ package com.cafs92.devolveai.controller;
 import com.cafs92.devolveai.model.Item;
 import com.cafs92.devolveai.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,4 +27,13 @@ public class ItemController {
                                                         ResponseEntity.ok().body(item))
                                                 .orElse(ResponseEntity.notFound().build());
     }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public Item addItem(@RequestBody Item item){
+        return itemRepository.save(item);
+    }
+
+
+
 }
